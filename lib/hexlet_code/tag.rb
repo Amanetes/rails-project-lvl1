596605a6f4006c8965815af1755129e5db058d4b
Sergey <ser_8904@mail.ru>
2022-05-16 10:56:09 +0300

    fix tag constants naming

--- lib/hexlet_code/tag.rb
+++ lib/hexlet_code/tag.rb
@@ -2,9 +2,9 @@
 
 module HexletCode
   class Tag
-    PAIRED = %w[div label form span p textarea].freeze
-    UNPAIRED = %w[br hr img input].freeze
-    SUPPORTED_TAGS = PAIRED.union(UNPAIRED).freeze
+    PAIRED_TAGS = %w[div label form span p textarea].freeze
+    UNPAIRED_TAGS = %w[br hr img input].freeze
+    SUPPORTED_TAGS = PAIRED_TAGS.union(UNPAIRED_TAGS).freeze
     def self.build(tag_name, options = {})
       raise ::HexletCode::Error, "Tag: #{tag_name} is not supported" unless SUPPORTED_TAGS.include?(tag_name)
 
@@ -13,7 +13,7 @@ module HexletCode
       opening_tag = tag_attributes.empty? ? "<#{tag_name}>" : "<#{tag_name} #{tag_attributes}>"
       closing_tag = "</#{tag_name}>"
 
-      PAIRED.include?(tag_name) ? "#{opening_tag}#{yield if block_given?}#{closing_tag}" : opening_tag
+      PAIRED_TAGS.include?(tag_name) ? "#{opening_tag}#{yield if block_given?}#{closing_tag}" : opening_tag
     end
   end
 end
